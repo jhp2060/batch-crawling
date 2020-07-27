da98d59e7abf9772bec98cf972f5ab771a6d176f
jhp2060 <jhp2060@gmail.com>
2020-07-27 16:49:56 +0900

    feat(etc): initial commit

---
+++ src/main/java/com/ddingdongsogang/batchcrawling/job/SimpleJobConfiguration.java
@@ -0,0 +1,13 @@
+package com.ddingdongsogang.batchcrawling.job;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class SimpleJobConfiguration {
+
+}
